18d81442eaf188e89be127f3b328a14a559837da
DimXlp <dchlop15@gmail.com>
2025-08-08 20:18:50 +0300

    initial commit

---
+++ src/main/java/com/dimxlp/kfrecalculator/KfrecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.dimxlp.kfrecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfrecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfrecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimxlp/kfrecalculator/KfrecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimxlp.kfrecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfrecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
